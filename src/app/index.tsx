0debcb295d81379c9672b0cc1a88c7e6e07cb44a
Nilesh <nileshdadab@gmail.com>
2025-08-11 13:57:52 +0530

    fix: update redirect path in HomeScreen and restore RootLayout component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,14 +3,5 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
-  return <Redirect href="/(home)/" />;
+  return <Redirect href="/(home)/(tabs)" />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
