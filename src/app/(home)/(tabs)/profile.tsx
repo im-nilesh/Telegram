81b890daa338ceae4eaa31ba2e9ab4ef53f61a44
Nilesh <nileshdadab@gmail.com>
2025-08-11 13:06:48 +0530

    refactor: restructure app to use expo-router and implement tab navigation
    - Removed App.tsx and replaced with a new routing structure using expo-router.
    - Updated app.json to include app scheme and added expo-router as a plugin.
    - Modified package.json to set the main entry point to expo-router/entry and added necessary dependencies.
    - Created layout and tab navigation components for home and profile screens.
    - Implemented basic screens for main tab and profile with placeholder text.

---
+++ src/app/(home)/(tabs)/profile.tsx
@@ -0,0 +1,5 @@
+import { View, Text, StatusBar } from "react-native";
+
+export default function MainTabScreen() {
+  return <Text>Profile</Text>;
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { Redirect } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return <Redirect href="/(home)/" />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
